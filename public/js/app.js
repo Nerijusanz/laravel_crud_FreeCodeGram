1eb1685ea12a62db602e85b040db53ee03ddd7b4
nerijus-ubuntu <nerijusanz@gmail.com>
2019-07-04 15:26:12 +0300

    public/js/app.js file compiled

--- public/js/app.js
+++ public/js/app.js
@@ -1907,38 +1907,40 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     followUser: function () {
       var _followUser = _asyncToGenerator(
       /*#__PURE__*/
-      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee(e) {
         var res;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                _context.prev = 0;
-                _context.next = 3;
+                // console.log(this.userId);
+                e.stopPropagation();
+                _context.prev = 1;
+                _context.next = 4;
                 return axios.post("/follow/".concat(this.userId));
 
-              case 3:
+              case 4:
                 res = _context.sent;
                 //props: user-id
                 console.log(res.data);
-                _context.next = 11;
+                _context.next = 12;
                 break;
 
-              case 7:
-                _context.prev = 7;
-                _context.t0 = _context["catch"](0);
+              case 8:
+                _context.prev = 8;
+                _context.t0 = _context["catch"](1);
                 console.log(_context.t0);
                 if (_context.t0.response.status == 401) window.location = "/login";
 
-              case 11:
+              case 12:
               case "end":
                 return _context.stop();
             }
           }
-        }, _callee, this, [[0, 7]]);
+        }, _callee, this, [[1, 8]]);
       }));
 
-      function followUser() {
+      function followUser(_x) {
         return _followUser.apply(this, arguments);
       }
 
@@ -37955,12 +37957,12 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", [
     _c(
-      "button",
+      "a",
       {
         staticClass: "btn btn-primary ml-4",
         on: {
           click: function($event) {
-            return _vm.followUser()
+            return _vm.followUser($event)
           }
         }
       },
